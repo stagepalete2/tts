55e4460dd8f7f21f2127199faf510b230a91d5be
stagepalete <221744@astanait.edu.kz>
2026-02-28 16:37:58 +0500

    Added settings page route

--- src/router.jsx
+++ src/router.jsx
@@ -6,6 +6,7 @@ import NotFound from '@/pages/Error/NotFound/NotFound'
 import History from '@/pages/History/History'
 import Home from '@/pages/Home/Home'
 import Projects from '@/pages/Projects/Projects'
+import Settings from '@/pages/Settings/Settings'
 import TTSCloud from '@/pages/TTSCloud/TTSCloud'
 import TTSGemini from '@/pages/TTSGemini/TTSGemini'
 import Voices from '@/pages/Voices/Voices'
@@ -45,6 +46,11 @@ const router = createBrowserRouter([
         path: "/tts-voices", 
         element: <Voices />,
       },
+
+      {
+        path: '/settings',
+        element: <Settings />
+      }
     ],
   },
 ]);
